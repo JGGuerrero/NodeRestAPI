91687ddf0ac84d97e1539c1fcba83ff446fcca66
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2020-04-12 12:25:13 -0700

    finished get for car model

--- dist/definitions/models/car.model.d.ts
+++ dist/definitions/models/car.model.d.ts
@@ -1,5 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 export declare class Car {
     _model: any;
     constructor(norm: any);
+    getAllCars(model: any): (req: Request<import("../../../../../../../../Users/Joshuah/Desktop/Fresno State Classes/IS183/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../../../Users/Joshuah/Desktop/Fresno State Classes/IS183/ExamAPI/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
+    getCarById(model: any): (req: Request<import("../../../../../../../../Users/Joshuah/Desktop/Fresno State Classes/IS183/ExamAPI/node_modules/@types/express-serve-static-core").ParamsDictionary, any, any, import("../../../../../../../../Users/Joshuah/Desktop/Fresno State Classes/IS183/ExamAPI/node_modules/@types/express-serve-static-core").Query>, res: Response<any>, next: NextFunction) => Promise<void>;
     model: any;
 }
