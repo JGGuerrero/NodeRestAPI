2e27b224e0e165a7c6741ed38355883fabdbd513
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2020-04-12 10:51:50 -0700

    push day one rest api

--- dist/seed/user.seed.js
+++ dist/seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "jgguerrero@mail.fresnostate.edu",
+  "username": "jgguerrero",
+  "password": "password123abc",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
